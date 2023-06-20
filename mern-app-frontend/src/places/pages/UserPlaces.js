5474eeb94986daf53e153cb602cb72ace10d0b11
yurisokolovicz <yurisokolovicz@gmail.com>
2023-06-20 15:49:04 -0300

    Loading Places by User Id

--- mern-app-frontend/src/places/pages/UserPlaces.js
+++ mern-app-frontend/src/places/pages/UserPlaces.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
-
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl:
-            'https://www.esbnyc.com/sites/default/files/styles/small_feature/public/2022-06/iStock-937427130%20%281%29.jpg?itok=osCyvhw5.jpg',
-        address: '20 W 34th St., New York, NY 10001',
-        location: { lat: 40.7484405, lng: -73.9878531 },
-        creator: 'u1'
-    },
-    {
-        id: 'p2',
-        title: 'Emp. State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageUrl:
-            'https://www.esbnyc.com/sites/default/files/styles/small_feature/public/2022-06/iStock-937427130%20%281%29.jpg?itok=osCyvhw5.jpg',
-        address: '20 W 34th St., New York, NY 10001',
-        creator: 'u2'
-    }
-];
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 const UserPlaces = () => {
+    const [loadedPlaces, setLoadedPlaces] = useState();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
     const userId = useParams().userId;
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
-    return <PlaceList items={loadedPlaces} />;
+
+    // useEffect to send the http request when the component render, but not when it re-render.
+    // The sendRequest will never be recreated because it is wrapped in useCallback, so it will never trigger the useEffect function to re-render. The function will only render once when the component is mounted.
+    useEffect(() => {
+        const fetchPlaces = async () => {
+            try {
+                // GET is the default
+                const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`);
+                setLoadedPlaces(responseData.places);
+            } catch (err) {}
+        };
+        fetchPlaces();
+    }, [sendRequest, userId]);
+
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+        </React.Fragment>
+    );
 };
 export default UserPlaces;
 
