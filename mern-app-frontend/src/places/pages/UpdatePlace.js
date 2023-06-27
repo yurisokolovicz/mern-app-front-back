bb8b91d312c7863eaee5e81359344768b37b3786
yurisokolovicz <yurisokolovicz@gmail.com>
2023-06-27 15:49:44 -0300

    Using Tokens to Update and Delete Places

--- mern-app-frontend/src/places/pages/UpdatePlace.js
+++ mern-app-frontend/src/places/pages/UpdatePlace.js
@@ -67,7 +67,7 @@ const UpdatePlace = () => {
                     title: formState.inputs.title.value,
                     description: formState.inputs.description.value
                 }),
-                { 'Content-Type': 'application/json' }
+                { 'Content-Type': 'application/json', Authorization: 'Bearer ' + auth.token }
             );
             history.push('/' + auth.userId + '/places');
         } catch (err) {}
