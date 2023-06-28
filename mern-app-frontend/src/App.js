bcacb5111e6cbb241e59a60ea7c1ca1a0c3891bd
yurisokolovicz <yurisokolovicz@gmail.com>
2023-06-28 14:34:24 -0300

    Creating .env.production - and Code Splitting to React

--- mern-app-frontend/src/App.js
+++ mern-app-frontend/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react'; // Suspense it is required to make React.lazy to work
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
+// import Users from './user/pages/Users';
+// import NewPlace from './places/pages/NewPlace';
+// import UserPlaces from './places/pages/UserPlaces';
+// import UpdatePlace from './places/pages/UpdatePlace';
+// import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
-import UserPlaces from './places/pages/UserPlaces';
-import UpdatePlace from './places/pages/UpdatePlace';
-import Auth from './user/pages/Auth';
 import { AuthContext } from './shared/context/auth-context';
 import { useAuth } from './shared/hooks/auth-hook';
+import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
+
+// This makes Users to dont be imported automatically when App.js render but only when the User component is required
+const Users = React.lazy(() => import('./user/pages/Users'));
+const NewPlace = React.lazy(() => import('./places/pages/NewPlace'));
+const UserPlaces = React.lazy(() => import('./places/pages/UserPlaces'));
+const UpdatePlace = React.lazy(() => import('./places/pages/UpdatePlace'));
+const Auth = React.lazy(() => import('./user/pages/Auth'));
 
 const App = () => {
     const { token, login, logout, userId } = useAuth();
@@ -54,7 +62,17 @@ const App = () => {
         <AuthContext.Provider value={{ isLoggedIn: !!token, token: token, userId: userId, login: login, logout: logout }}>
             <Router>
                 <MainNavigation />
-                <main>{routes}</main>
+                <main>
+                    <Suspense
+                        fallback={
+                            <div className="center">
+                                <LoadingSpinner />
+                            </div>
+                        }
+                    >
+                        {routes}
+                    </Suspense>
+                </main>
             </Router>
         </AuthContext.Provider>
     );
